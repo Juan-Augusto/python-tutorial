d32d6bac49a6121a980b1265742c86a5a3f21e95
juan-augusto <juanaugusto1@live.com>
2022-02-09 15:40:37 -0300

    Loop

--- Function/Function.py
+++ Function/Function.py
@@ -1,4 +1,6 @@
 def hello_world():
-        print('Hello World!');
-hello_world()
+        name = 'Juan'
+        print('Hello World! Ass.:' + name);
+hello_world();
+
 
---
+++ Loop/LoopFunction.py
@@ -0,0 +1,11 @@
+#for <variable> in <iterable>:
+#   action
+for letter in 'Hello world':
+    print(letter);
+
+myList = [1, 2, 3, 4, 5];
+for item in myList:
+    print((item));
+
+print(len(myList));
+
