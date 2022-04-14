aba4673ac82078d6fede79433bf5a319b9a1b767
unknown <Darede@DAREDE-001017.darede.corp>
2022-04-14 13:38:14 -0300

    Sum function

---
+++ Sum/Sum.py
@@ -0,0 +1,4 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+S = first_number + second_number
+print("The sum between {} and {} is: {}".format(first_number, second_number, S))
