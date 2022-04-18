c47f618f891222ea6c73c00e9cb4947f8b1a58bd
Juan Augusto <juanaugusto1@live.com>
2022-04-18 11:38:23 -0300

    Double, triple and square root of a number exercise

---
+++ DoubleTripleSquareRoot/DoubleTripleSquareRoot.py
@@ -0,0 +1,4 @@
+main_value = int(input("Enter the main number: "))
+print("The double of the number is: {}".format(main_value * 2))
+print("The triple of the number is: {}".format(main_value * 3))
+print("The square root of the number is: {}".format(main_value ** (1/2)))
