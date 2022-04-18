7502c28b505d2204fd3a9c4bec87a08bc82445cd
Juan Augusto <juanaugusto1@live.com>
2022-04-18 11:35:04 -0300

    Predecessor and successor exercise

---
+++ PredecessorSuccessor/PredecessorSuccessor.py
@@ -0,0 +1,3 @@
+main_value = int(input("Enter the main number: "))
+print("The predecessor of the number is: {}".format(main_value - 1))
+print("The successor of the number is: {}".format(main_value + 1))
