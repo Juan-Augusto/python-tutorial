6087d380343b9642ff3e23818b74ffe896c72101
Juan Augusto <juanaugusto1@live.com>
2022-04-18 11:40:24 -0300

    Average exercise

---
+++ Average/Average.py
@@ -0,0 +1,4 @@
+first_grade = int(input("Enter the first grade: "))
+second_grade = int(input("Enter the second grade: "))
+
+print("The average between {} and {} is: {}".format(first_grade, second_grade, (first_grade + second_grade) / 2))
