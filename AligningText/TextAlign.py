6f33e4becaad4e5eca89e59a5cc15c938afa7ca5
unknown <Darede@DAREDE-001017.darede.corp>
2022-04-18 11:11:59 -0300

    Text align exercise

---
+++ AligningText/TextAlign.py
@@ -0,0 +1,4 @@
+nome = input('Qual é o seu nome? ')
+print('Seu nome é {:=>20}! ALINHADO À DIREITA'.format(nome))
+print('Seu nome é {:=<20}! ALINHADO À ESQUERDA'.format(nome))
+print('Seu nome é {:=^20}! ALINHADO CENTRALIZADO'.format(nome))
