ca07454efcfa8f5a97aad3bbaf37c7b95728bccc
Juan Augusto <juanaugusto1@live.com>
2022-04-18 18:05:12 -0300

    Sine, cosine and tangent exercise

---
+++ SinCos/SinCos.py
@@ -0,0 +1,6 @@
+from math import sin, cos, tan
+
+main_angle = float(input("Enter the main angle: "))
+print("The sine of the main angle is {}".format(sin(main_angle)))
+print("The cosine of the main angle is {}".format(cos(main_angle)))
+print("The tangent of the main angle is {}".format(tan(main_angle)))
