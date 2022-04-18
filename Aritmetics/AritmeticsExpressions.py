afb7be95e838401603cdf90b130c18f09e107257
unknown <Darede@DAREDE-001017.darede.corp>
2022-04-18 11:28:54 -0300

    Aritmetics exercise

---
+++ Aritmetics/AritmeticsExpressions.py
@@ -0,0 +1,15 @@
+first_value = int(input("Enter the first number: "))
+second_value = int(input("Enter the second number: "))
+sum = first_value + second_value
+multiplication = first_value * second_value
+subtraction = first_value - second_value
+division = first_value / second_value
+division_int = first_value // second_value
+modulus = first_value % second_value
+print("The sum between {} and {} is: {}".format(first_value, second_value, sum))
+print("The multiplication between {} and {} is: {}".format(first_value, second_value, multiplication))
+print("The subtraction between {} and {} is: {}".format(first_value, second_value, subtraction))
+print("The division between {} and {} is: {}".format(first_value, second_value, division))
+print("The division between {} and {} is: {}".format(first_value, second_value, division_int))
+print("The modulus between {} and {} is: {}".format(first_value, second_value, modulus))
+print("{} powered by {} is: {}".format(first_value, second_value, first_value ** second_value))
